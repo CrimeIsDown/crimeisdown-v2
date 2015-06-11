398e2f2f90c128465c43d36617a37f82b4ca32da
Eric Tendian <erictendian@gmail.com>
2015-06-10 22:37:34 -0500

    Adding more logic to the lookup function

--- src/components/map/map.controller.js
+++ src/components/map/map.controller.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('crimeisdown')
-  .controller('MapCtrl', function ($scope, $http) {
+  .controller('MapCtrl', function ($scope, mapUtils) {
     // this should not be done
     String.prototype.toProperCase = function () {
       return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
     };
 
-    $scope.map = null;
+    $scope.map = mapUtils.createMap(document.getElementById('map-canvas'));
+    mapUtils.loadLayers($scope.map);
     $scope.location = {
       meta: {formatted_address: '', latitude: '', longitude: '', neighborhood: '', community_area: ''},
       police: {beat: '', zone: '', district: '', area: ''},
@@ -15,109 +16,9 @@ angular.module('crimeisdown')
       stats: {population: '', homicides: '', shootings: '', income: ''}
     };
 
-    $scope.polyDesc = '';
-
-    initializeMap();
-
     $scope.lookupAddress = function () {
-      geocoder.geocode({'address': $scope.address, bounds: chicago}, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          $scope.location.meta.formatted_address = results[0].formatted_address;
-          $scope.location.meta.latitude = results[0].geometry.location.lat().toFixed(6);
-          $scope.location.meta.longitude = results[0].geometry.location.lng().toFixed(6);
-          $scope.map.setCenter(results[0].geometry.location);
-          var marker = new google.maps.Marker({
-              map: $scope.map,
-              position: results[0].geometry.location
-          });
-          $scope.map.setCenter(marker.position);
-          $scope.map.setZoom(15);
-        } else {
-          alert("Geocode was not successful for the following reason: " + status);
-        }
-      });
+      $scope.location = mapUtils.lookupAddress($scope.address);
+      console.log($scope.location);
     };
-
-
-    function initializeMap () {
-      var mapOptions = {
-        center: {lat: 41.8369, lng: -87.6847},
-        zoom: 10
-      };
-      $scope.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-      var geocoder = new google.maps.Geocoder();
-      var chicago = new google.maps.Circle({center: mapOptions.center, radius: 50000}).getBounds()
-
-      var communityAreasLayer = new google.maps.Data();
-      var neighborhoodsLayer = new google.maps.Data();
-      var policeDistrictsLayer = new google.maps.Data();
-
-      communityAreasLayer.setStyle({
-        fillOpacity: 0.0,
-        strokeColor: '#333',
-        strokeWeight: 2
-      });
-      communityAreasLayer.addListener('click', function (event) {
-        var result = event.feature.getProperty('COMMUNITY').toProperCase();
-        $scope.$apply(function () {
-          $scope.location.meta.community_area = result;
-        });
-      });
-      $http.get('assets/data/community_areas.json')
-        .then(function (res) {
-          var json = new GeoJSON(res.data);
-          json.forEach(function (value) {
-            console.log(value[0]);
-            var feature = new google.maps.Data.Feature();
-            feature.setGeometry(value[0]);
-            communityAreasLayer.add(feature);
-          });
-        });
-      setTimeout(function () {
-        communityAreasLayer.forEach(function (feature) {
-          console.log(feature);
-          var point = new google.maps.LatLng(41.888872, -87.627583);
-          if (google.maps.geometry.poly.containsLocation(point, feature.getGeometry())) {
-            console.log('found it!');
-            console.log(feature);
-          }
-        });
-      }, 5000);
-
-
-
-      neighborhoodsLayer.loadGeoJson('assets/data/neighborhoods.json');
-      neighborhoodsLayer.setStyle({
-        fillOpacity: 0.0,
-        strokeColor: '#CCC',
-        strokeWeight: 1
-      });
-      neighborhoodsLayer.addListener('click', function (event) {
-        var result = event.feature.getProperty('PRI_NEIGH').toProperCase() + ' / ' + event.feature.getProperty('SEC_NEIGH').toProperCase();
-        $scope.$apply(function () {
-          $scope.location.meta.neighborhood = result;
-        });
-      });
-
-      policeDistrictsLayer.loadGeoJson('assets/data/police_districts.json');
-      policeDistrictsLayer.setStyle({
-        fillOpacity: 0.0,
-        strokeColor: 'blue',
-        strokeWeight: 3
-      });
-      policeDistrictsLayer.addListener('click', function (event) {
-        var result = event.feature.getProperty('DIST_LABEL').toLowerCase();
-        $scope.$apply(function () {
-          $scope.location.police.district = result;
-        });
-      });
-
-      communityAreasLayer.setMap($scope.map);
-      neighborhoodsLayer.setMap($scope.map);
-      policeDistrictsLayer.setMap($scope.map);
-
-
-    }
 
   });
---
+++ src/components/map/map.service.js
@@ -0,0 +1,159 @@
+'use strict';
+
+angular.module('crimeisdown')
+  .factory('mapUtils', ['$http', function ($http) {
+    var mapService = {};
+
+    var polygons = {
+      communityAreas: [],
+      neighborhoods: [],
+      policeDistricts: [],
+      policeBeats: []
+    };
+
+    var mapOptions = {
+      center: {lat: 41.8369, lng: -87.6847},
+      zoom: 10
+    };
+    var chicago = new google.maps.Circle({center: mapOptions.center, radius: 50000}).getBounds();
+    var geocoder = new google.maps.Geocoder();
+    var map;
+
+    mapService.createMap = function (element) {
+      map = new google.maps.Map(element, mapOptions);
+      return map;
+    };
+
+    mapService.lookupAddress = function (address) {
+      var location = {meta: {}, police: {}, fire: {}, stats: {}};
+      geocoder.geocode({'address': address, bounds: chicago}, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          var point = results[0].geometry.location;
+          location.meta.formatted_address = results[0].formatted_address;
+          location.meta.latitude = point.lat().toFixed(6);
+          location.meta.longitude = point.lng().toFixed(6);
+          map.setCenter(point);
+          var marker = new google.maps.Marker({
+              map: map,
+              position: point
+          });
+          map.setCenter(marker.position);
+          map.setZoom(15);
+
+          polygons.communityAreas.forEach(function (poly) {
+            if (google.maps.geometry.poly.containsLocation(point, poly)) {
+              location.meta.community_area = poly.geojsonProperties.COMMUNITY;
+            }
+          });
+
+          polygons.neighborhoods.forEach(function (poly) {
+            if (google.maps.geometry.poly.containsLocation(point, poly)) {
+              location.meta.neighborhood = poly.geojsonProperties.PRI_NEIGH;
+            }
+          });
+
+          polygons.policeDistricts.forEach(function (poly) {
+            if (google.maps.geometry.poly.containsLocation(point, poly)) {
+              location.police.district = poly.geojsonProperties.DIST_LABEL.toLowerCase();
+
+              var zones = {'1': ['16', '17'], '2': ['19'], '3': ['12', '14'], '4': ['1', '18'], '5': ['2'], '6': ['7', '8'], '7': ['3'], '8': ['4', '6'], '9': ['5', '22'], '10': ['10', '11'], '11': ['20', '24'], '12': ['15', '25'], '13': ['9']};
+              var areas = {'North': ['11', '14', '15', '16', '17', '19', '20', '24'], 'Central': ['1', '2', '3', '8', '9', '10', '12', '18'], 'South': ['4', '5', '6', '7', '22']}
+
+              for (var key in zones) {
+                if ($.inArray(poly.geojsonProperties.DIST_NUM, zones[key])>-1) {
+                  location.police.zone = key;
+                }
+              }
+              for (var key in areas) {
+                if ($.inArray(poly.geojsonProperties.DIST_NUM, areas[key])) {
+                  location.police.area = key;
+                }
+              }
+            }
+          });
+
+          polygons.policeBeats.forEach(function (poly) {
+            if (google.maps.geometry.poly.containsLocation(point, poly)) {
+              location.police.beat = poly.geojsonProperties.BEAT_NUM;
+            }
+          });
+        } else {
+          alert("Geocode was not successful for the following reason: " + status);
+        }
+      });
+      return location;
+    };
+
+    mapService.loadLayers = function (map) {
+      var communityAreasLayer = loadCommunityAreas();
+      communityAreasLayer.setMap(map);
+
+      var neighborhoodsLayer = loadNeighborhoods();
+      neighborhoodsLayer.setMap(map);
+
+      var policeDistrictsLayer = loadPoliceDistricts();
+      policeDistrictsLayer.setMap(map);
+
+      var policeBeatsLayer = loadPoliceBeats();
+    }
+
+    function loadCommunityAreas() {
+      var communityAreasLayer = new google.maps.Data();
+      loadGeoJSON('assets/data/community_areas.json', communityAreasLayer, polygons.communityAreas);
+      communityAreasLayer.setStyle({fillOpacity: 0.0, strokeColor: '#333', strokeWeight: 2});
+      return communityAreasLayer;
+    }
+
+    function loadNeighborhoods() {
+      var neighborhoodsLayer = new google.maps.Data();
+      loadGeoJSON('assets/data/neighborhoods.json', neighborhoodsLayer, polygons.neighborhoods);
+      neighborhoodsLayer.setStyle({fillOpacity: 0.0, strokeColor: '#CCC', strokeWeight: 1});
+      return neighborhoodsLayer;
+    }
+
+    function loadPoliceDistricts() {
+      var policeDistrictsLayer = new google.maps.Data();
+      loadGeoJSON('assets/data/police_districts.json', policeDistrictsLayer, polygons.policeDistricts);
+      policeDistrictsLayer.setStyle({fillOpacity: 0.0, strokeColor: 'blue', strokeWeight: 3});
+      return policeDistrictsLayer;
+    }
+
+    function loadPoliceBeats() {
+      var policeBeatsLayer = new google.maps.Data();
+      loadGeoJSON('assets/data/police_beats.json', policeBeatsLayer, polygons.policeBeats);
+      policeBeatsLayer.setStyle({fillOpacity: 0.0, strokeColor: 'blue', strokeWeight: 3});
+      return policeBeatsLayer;
+    }
+
+    function loadGeoJSON (url, layer, list) {
+      $http.get(url)
+        .then(function (res) {
+          var json = new GeoJSON(res.data);
+          layer.addGeoJson(res.data);
+          json.forEach(function (value) {
+            list.push(value[0]);
+          });
+        });
+    }
+
+    // communityAreasLayer.addListener('click', function (event) {
+    //   var result = event.feature.getProperty('COMMUNITY').toProperCase();
+    //   $scope.$apply(function () {
+    //     $scope.location.meta.community_area = result;
+    //   });
+    // });
+    // neighborhoodsLayer.addListener('click', function (event) {
+    //   var result = event.feature.getProperty('PRI_NEIGH').toProperCase() + ' / ' + event.feature.getProperty('SEC_NEIGH').toProperCase();
+    //   $scope.$apply(function () {
+    //     $scope.location.meta.neighborhood = result;
+    //   });
+    // });
+    // policeDistrictsLayer.addListener('click', function (event) {
+    //   var result = event.feature.getProperty('DIST_LABEL').toLowerCase();
+    //   $scope.$apply(function () {
+    //     $scope.location.police.district = result;
+    //   });
+    // });
+
+    return mapService;
+  }]);
