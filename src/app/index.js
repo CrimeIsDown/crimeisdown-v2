2ad217c7155345e8a5bc8faba025a393e417257e
Eric Tendian <erictendian@gmail.com>
2015-06-11 19:26:06 -0500

    Moving around some static pages

--- src/app/index.js
+++ src/app/index.js
@@ -8,14 +8,14 @@ angular.module('crimeisdown', ['ngResource', 'ui.router', 'ui.bootstrap'])
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       })
-      .state('about', {
+      .state('aboutUs', {
         url: '/about',
         templateUrl: 'app/static/about.html',
         controller: 'StaticCtrl'
       })
-      .state('takeAction', {
-        url: '/take-action',
-        templateUrl: 'app/static/take-action.html',
+      .state('live', {
+        url: '/is-crime-down',
+        templateUrl: 'app/static/live.html',
         controller: 'StaticCtrl'
       })
       .state('gettingStarted', {
@@ -26,11 +26,6 @@ angular.module('crimeisdown', ['ngResource', 'ui.router', 'ui.bootstrap'])
       .state('ethics', {
         url: '/ethics',
         templateUrl: 'app/static/ethics.html',
-        controller: 'StaticCtrl'
-      })
-      .state('intro', {
-        url: '/intro',
-        templateUrl: 'app/static/intro.html',
         controller: 'StaticCtrl'
       })
       ;
