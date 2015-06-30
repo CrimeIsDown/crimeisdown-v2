94aba053de8bd73a86f8fef0af3f3373c814d2a2
Eric Tendian <erictendian@gmail.com>
2015-06-29 20:26:44 -0500

    Fixing Twitter widgets

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('crimeisdown')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $timeout) {
+    try {
+      $timeout = twttr.widgets.load();
+    } catch (e) {}
     $scope.alerts = [
-      { type: 'info', msg: "<strong>Welcome to the new CrimeIsDown.com!</strong> We've revamped the site to focus on our scanner tools, as well as added a couple new features. More updates to come!" }
+      { type: 'info', msg: "<strong>Welcome to the new CrimeIsDown.com!</strong> There have been a lot of changes to the site and we're still improving it. Email Eric (<a href='mailto:eric@crimeisdown.com'>eric@crimeisdown.com</a>) with your feedback/suggestions. Any <a href='https://www.paypal.com/cgi-bin/webscr?cmd=_donations&amp;business=4SHEMCHJPV8KY&amp;lc=US&amp;item_name=CrimeIsDown%2ecom&amp;currency_code=USD&amp;bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted' target='_blank'>donations</a> would be appreciated so we can pay for additional hosting costs." }
     ];
     $scope.closeAlert = function(index) {
       $scope.alerts.splice(index, 1);
--- src/app/static/static.controller.js
+++ src/app/static/static.controller.js
@@ -1,5 +1,8 @@
 'use strict';
 
 angular.module('crimeisdown')
-  .controller('StaticCtrl', function () {
+  .controller('StaticCtrl', function ($timeout) {
+    try {
+      $timeout = twttr.widgets.load();
+    } catch (e) {}
   });
